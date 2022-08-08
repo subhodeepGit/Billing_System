5855be4a5dd48d0cd4b96222f49b38e0e9f5f6fe
SPriytesh <priytesh.shah@soulunileaders.com>
2022-08-08 11:48:40 +0530

    Reservation and customer changes

--- billing_system/billing_system/doctype/district/district.js
+++ billing_system/billing_system/doctype/district/district.js
@@ -1,20 +1,13 @@
 // Copyright (c) 2022, SOUL Limited and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Hotel', {
+frappe.ui.form.on('District', {
 	onload:function(frm)
     {
         frm.set_query("state", function() {
             return {
                 filters: {
                     "country":frm.doc.country
-                }
-            };
-        });
-		frm.set_query("district", function() {
-            return {
-                filters: {
-                    "state":frm.doc.state
                 }
             };
         });
---
+++ billing_system/billing_system/doctype/state/state.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2022, SOUL Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('State', {
+	//
+});
